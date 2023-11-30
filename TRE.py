62033e28ba6c8d1e45d6a1b489941337e5d1231d
Xinqiang Ding <xqding@umich.edu>
2023-11-30 16:53:31 -0500

    initial version that exchanges temperatures

--- TRE.py
+++ TRE.py
@@ -1,10 +1,7 @@
-__author__ = "Xinqiang Ding <xqding@umich.edu>"
-__date__ = "2020/05/09 02:20:46"
-
 import numpy as np
-import simtk.openmm.app as app
-import simtk.openmm as omm
-import simtk.unit as unit
+import openmm.app as app
+import openmm as omm
+import openmm.unit as unit
 import math
 import ray
 
@@ -13,12 +10,20 @@ class TREActor:
     """
     TREActor is a class for running molecular dynamics in a replica.
     """
-    def __init__(self, system, integrator, platform_name,
-                 temperature, topology,
-                 dcd_file_name, initial_positions = None):
-        """ Construct an instance of TREActor which holding all the information
+
+    def __init__(
+        self,
+        system,
+        integrator,
+        platform_name,
+        temperature,
+        topology,
+        dcd_file_name,
+        initial_positions=None,
+    ):
+        """Construct an instance of TREActor which holding all the information
         for running simulation in one replica.
-        
+
         Parameters
         ----------
         system : System
@@ -36,7 +41,7 @@ class TREActor:
         dcd_file_name : string
             the dcd file name where the trajectory is saved
         """
-        
+
         self.system = system
         self.integrator = integrator
         self.platform_name = platform_name
@@ -53,28 +58,31 @@ class TREActor:
             self.context.setPositions(self.initial_positions)
 
         ## open the dcd_file
-        file_handle = open(dcd_file_name, 'bw')
-        self.dcd_file = omm.app.dcdfile.DCDFile(file_handle, self.topology,
-                                                self.integrator.getStepSize())
-        
+        file_handle = open(dcd_file_name, "bw")
+        self.dcd_file = omm.app.dcdfile.DCDFile(
+            file_handle, self.topology, self.integrator.getStepSize()
+        )
+
         ## record temperature and potential energy each time the actor
         ## saves the state
         self.temperature_record = []
         self.potential_energy_record = []
-        
+
     def run_md(self, num_steps):
         self.integrator.step(num_steps)
 
     def get_potential_energy(self):
-        state = self.context.getState(getEnergy = True)
-        potential_energy = state.getPotentialEnergy()
+        state = self.context.getState(getEnergy=True)
+        potential_energy = state.getPotentialEnergy().value_in_unit(
+            unit.kilojoule_per_mole
+        )
         return potential_energy
 
     def update_temperature(self, temperature):
-        state = self.context.getState(getVelocities = True)
+        state = self.context.getState(getVelocities=True)
         velocities = state.getVelocities()
-        velocities = velocities * math.sqrt(temperature/self.temperature)        
-        self.context.setVelocities(velocities)        
+        velocities = velocities * math.sqrt(temperature / self.temperature)
+        self.context.setVelocities(velocities)
         self.temperature = temperature
         self.integrator.setTemperature(temperature)
 
@@ -82,7 +90,7 @@ class TREActor:
         return self.temperature
 
     def save_state(self):
-        state = self.context.getState(getPositions = True)
+        state = self.context.getState(getPositions=True)
         positions = state.getPositions()
         self.dcd_file.writeModel(positions)
         self.temperature_record.append(self.temperature)
@@ -93,32 +101,37 @@ class TREActor:
 
     def get_potential_energy_record(self):
         return self.potential_energy_record
-    
+
     def get_checkpoint(self):
-        state = self.context.getState(getPositions = True,
-                                      getVelocities = True)
-        positions = state.getPositions()        
-        velocities = state.getVelocities()
+        state = self.context.getState(getPositions=True, getVelocities=True)
+        positions = state.getPositions().values_in_unit(unit.nanometer)
+        velocities = state.getVelocities().values_in_unit(
+            unit.nanometer / unit.picoseconds
+        )
         temperature = self.temperature
 
-        return {'positions': positions,
-                'velocities': velocities,
-                'temperature': temperature}
-    
+        return {
+            "positions": positions,
+            "velocities": velocities,
+            "temperature": temperature,
+        }
+
     def load_checkpoint(self, checkpoint):
-        self.integrator.setTemperature(checkpoint['temperature'])        
-        self.context.setPositions(checkpoint['positions'])
-        self.context.setVelocities(checkpoint['velocities'])        
+        self.integrator.setTemperature(checkpoint["temperature"])
+        self.context.setPositions(checkpoint["positions"])
+        self.context.setVelocities(checkpoint["velocities"])
 
     def set_parameter(self, name, value):
         self.context.setParameter(name, value)
-    
-class TRE():
-    """ 
+
+
+class TRE:
+    """
     A class for running temperature replica exchange simulation
     """
+
     def __init__(self, actors):
-        """ Construct TRE with a list of actors, each of which is a replica
+        """Construct TRE with a list of actors, each of which is a replica
 
         Parameters
         ----------
@@ -128,21 +141,26 @@ class TRE():
         self.actors = actors
 
     def _exchange_temperature(self):
-        potential_energies = ray.get([actor.get_potential_energy.remote()
-                                      for actor in self.actors])
-        actor_temperatures = ray.get([actor.get_temperature.remote()
-                                      for actor in self.actors])
-        kbTs = [unit.BOLTZMANN_CONSTANT_kB*unit.AVOGADRO_CONSTANT_NA*(T*unit.kelvin)
-                for T in actor_temperatures]
+        potential_energies = ray.get(
+            [actor.get_potential_energy.remote() for actor in self.actors]
+        )
+        actor_temperatures = ray.get(
+            [actor.get_temperature.remote() for actor in self.actors]
+        )
+        kbTs = [
+            unit.BOLTZMANN_CONSTANT_kB * unit.AVOGADRO_CONSTANT_NA * (T * unit.kelvin)
+            for T in actor_temperatures
+        ]
 
-        for idx_replica in range(len(self.actors)-1):
+        for idx_replica in range(len(self.actors) - 1):
             actor_sort_idx = np.argsort(actor_temperatures)
 
             i = actor_sort_idx[idx_replica]
             j = actor_sort_idx[idx_replica + 1]
 
-            delta = (potential_energies[i]-potential_energies[j])* \
-                    (1./kbTs[i] - 1./kbTs[j])
+            delta = (potential_energies[i] - potential_energies[j]) * (
+                1.0 / kbTs[i] - 1.0 / kbTs[j]
+            )
 
             if np.random.rand() < np.exp(delta):
                 tmp = actor_temperatures[i]
@@ -154,31 +172,34 @@ class TRE():
 
     def run(self, num_steps, exchange_freq, save_freq):
         if exchange_freq < save_freq and save_freq % exchange_freq != 0:
-            raise ValueError("save_freq has to be divisible by exchange_freq when it is larger than exchange_freq")
+            raise ValueError(
+                "save_freq has to be divisible by exchange_freq when it is larger than exchange_freq"
+            )
         if exchange_freq > save_freq and exchange_freq % save_freq != 0:
-            raise ValueError("exchange_freq has to be divisible by save_freq when it is larger than save_freq")
+            raise ValueError(
+                "exchange_freq has to be divisible by save_freq when it is larger than save_freq"
+            )
 
-        
         if exchange_freq < save_freq:
-            num_iterations = num_steps // exchange_freq            
+            num_iterations = num_steps // exchange_freq
             for idx_iter in range(num_iterations):
                 if (idx_iter + 1) % 100 == 0:
-                    print("idx_iter: {}".format(idx_iter), flush = True)
+                    print("idx_iter: {}".format(idx_iter), flush=True)
 
                 [actor.run_md.remote(exchange_freq) for actor in self.actors]
-                if (idx_iter + 1) % (save_freq // exchange_freq) ==0:
-                    [actor.save_state.remote() for actor in self.actors]    
+                if (idx_iter + 1) % (save_freq // exchange_freq) == 0:
+                    [actor.save_state.remote() for actor in self.actors]
                 self._exchange_temperature()
-                
+
         else:
             num_iterations = num_steps // save_freq
             for idx_iter in range(num_iterations):
                 if (idx_iter + 1) % 100 == 0:
-                    print("idx_iter: {}".format(idx_iter), flush = True)
+                    print("idx_iter: {}".format(idx_iter), flush=True)
 
                 [actor.run_md.remote(save_freq) for actor in self.actors]
                 [actor.save_state.remote() for actor in self.actors]
-                if (idx_iter + 1) % (exchange_freq // save_freq) ==0:
+                if (idx_iter + 1) % (exchange_freq // save_freq) == 0:
                     self._exchange_temperature()
 
     def get_checkpoints(self):
@@ -186,6 +207,8 @@ class TRE():
         return checkpoints
 
     def load_checkpoints(self, checkpoints):
-        assert(len(checkpoints) == len(self.actors))
-        [self.actors[i].load_checkpoint.remote(checkpoints[i]) for i in range(len(checkpoints))]
-        
+        assert len(checkpoints) == len(self.actors)
+        [
+            self.actors[i].load_checkpoint.remote(checkpoints[i])
+            for i in range(len(checkpoints))
+        ]
